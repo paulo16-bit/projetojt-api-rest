7038875a8af8efbbe76bfe20e260d4f2c76fdfbe
enosh <enoshenrique008@gmail.com>
2025-10-02 17:10:52 -0300

    criação de CorsConfig e VendaDTO

--- src/main/java/com/ex/projetojt_api_rest/controller/VendaController.java
+++ src/main/java/com/ex/projetojt_api_rest/controller/VendaController.java
@@ -1,11 +1,22 @@
 package com.ex.projetojt_api_rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.ex.projetojt_api_rest.model.dto.VendaDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 public class VendaController {
+
+
+    @PostMapping("/vendas")
+    public ResponseEntity<?> postVendas(@RequestBody List<VendaDTO> vendas){
+        System.out.println(vendas);
+        return ResponseEntity.ok().build();
+    }
+
     @GetMapping("/vendas")
     public String getVendas() {
         return "Vendas";
--- src/main/java/com/ex/projetojt_api_rest/model/dto/VendaDTO.java
+++ src/main/java/com/ex/projetojt_api_rest/model/dto/VendaDTO.java
@@ -1,4 +1,17 @@
 package com.ex.projetojt_api_rest.model.dto;
 
-public record VendaDTO() {
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public record VendaDTO(
+        String idProduto,
+        String idCliente,
+        String nomeProduto,
+        String nomeCliente,
+        Integer qtdVendida,
+        Double valorUnit,
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDate dataVenda
+) {
 }
